2def4c23909c85922bbb01a1d2d287bd29b3431c
MemeMCNetwork <139055961+MemeMCNetwork@users.noreply.github.com>
2023-07-12 18:44:18 -0400

    Update and rename config.js.example to config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  TOKEN: "MTAwNjU2OTQ2Mzk0MTk1NTYyNQ.GTEk4m.zXCHMZBSDQMv36u8gaKqs3Q8AfE9QUGH1ev7Sw",
+  MAX_PLAYLIST_SIZE: 25,
+  STAY_TIME: 10,
+  DEFAULT_VOLUME: 100,
+  id: "1006569463941955625",
+  clientSecret: "V-9CHnzZCMuA3OuUGmdz60nfrj5FPBuE",
+  domain: "http://localhost",
+  port: 8080,
+  SOUNDCLOUD_CLIENT_ID: "",
+  DBCONNECTION: "MemeMCBot",
+  DEFAULTSETTINGS: {
+    prefix: "."
+  }
+}
